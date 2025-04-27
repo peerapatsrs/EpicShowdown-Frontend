65454dc09032c2f187de52863621924fbd7bdc52
Peerapat Surasing <p.surasing@hotmail.com>
2025-04-27 17:58:15 +0700

    Add HTTPS configuration and SSL certificates to Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: 'epicshowdown.com',
+		port: 443,
+		https: {
+			key: fs.readFileSync('certs/key.pem'),
+			cert: fs.readFileSync('certs/cert.pem'),
+		},
+	}
 });
